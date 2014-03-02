32d06e6fa80e845aaaa1c27854a1a255d7c71dd4
ndruger <snowtmp@gmail.com>
2014-03-03 02:10:26 +0900

    selenium test

--- build.sbt
+++ build.sbt
@@ -2,3 +2,5 @@
 scalaVersion := "2.10.1"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
+
+libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.35.0" % "test"
--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -1,20 +1,15 @@
-import collection.mutable.Stack
 import org.scalatest._
+import org.scalatest.selenium.WebBrowser
 
-class ExampleSpec extends FlatSpec with Matchers {
+class BlogSpec extends FlatSpec with ShouldMatchers with WebBrowser {
 
-  "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
-  }
+  implicit val webDriver: WebDriver = new HtmlUnitDriver
 
-  it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
-    a [NoSuchElementException] should be thrownBy {
-      emptyStack.pop()
-    } 
+  val host = "http://localhost/"
+
+  "The blog app home page" should "have the correct title" in {
+    go to (host + "index.html")
+    println pageTitle
+    pageTitle should be ("test")
   }
 }
