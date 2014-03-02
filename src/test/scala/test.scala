ee5591176e817b6ee842e77c9382a35621205143
ndruger <snowtmp@gmail.com>
2014-03-03 02:31:34 +0900

    add eventually

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -3,6 +3,7 @@ import org.scalatest.selenium.WebBrowser
 import org.scalatest.selenium._
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import concurrent.Eventually._
 
 class BlogSpec extends FlatSpec with ShouldMatchers with WebBrowser {
 
@@ -12,6 +13,6 @@ class BlogSpec extends FlatSpec with ShouldMatchers with WebBrowser {
 
   "The blog app home page" should "have the correct title" in {
     go to (host + "index.html")
-    pageTitle should be ("")
+    eventually { pageTitle should be ("") }
   }
 }
