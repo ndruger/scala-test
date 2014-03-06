f43a034b9e524389f17d6569fa2cf42697ef0818
ndruger <snowfullnight@gmail.com>
2014-03-07 04:20:55 +0900

    refactoring

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -3,17 +3,26 @@ import org.scalatest.selenium._
 import org.scalatest.time.Span
 import org.scalatest.time.Seconds
 
+
 class WebDriverTestSpec extends FlatSpec with Matchers with Chrome {
 
   val host = "http://localhost:3000/"
 
+  implicit class MyElement(self: Element) {
+    def click(): Unit = clickOn(self)
+  }
+  
+  def cssFind(css:String): Element = {
+    find(cssSelector(css)).get
+  }
+  
   "button" should "create result element" in {
     implicitlyWait(Span(5, Seconds))
 
     go to (host + "index.html")
     pageTitle should be ("test_title")
 
-    click on cssSelector("button")
-    find(cssSelector(".result")).get.text should be ("result_text")
+    cssFind("button").click()
+    cssFind(".result").text should be ("result_text")
   }
 }
